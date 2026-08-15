9151c13bc253afac1d853befcf2c2f3b7307cc14
LYH2263 <lyh2263@users.noreply.github.com>
2026-08-15 23:30:48 +0800

    fix: context.Value 键

--- internal/reqmeta/reqmeta.go
+++ internal/reqmeta/reqmeta.go
@@ -3,11 +3,15 @@ package reqmeta
 
 import "context"
 
+type key int
+
+const userKey key = 1
+
 func WithUser(ctx context.Context, user string) context.Context {
-	return context.WithValue(ctx, "user", user) // BUG: string key
+	return context.WithValue(ctx, userKey, user)
 }
 
 func User(ctx context.Context) string {
-	v, _ := ctx.Value("user").(string)
+	v, _ := ctx.Value(userKey).(string)
 	return v
 }
